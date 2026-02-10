819d2e79dca2429bca40e853fbf0873bb47c8d06
ibnu <ibnu.sina009@gmail.com>
2026-02-10 23:09:16 +0700

    fix animation

--- MetalBanner/ContentView.swift
+++ MetalBanner/ContentView.swift
@@ -26,22 +26,24 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Image(images[counter]).frame(height:200).padding()
-                    .opacity(opacity)
-                    .drawingGroup()
-                    .transition(self.diamondWave(size: 20))
-                    .onReceive(timer) { input in
-                        withAnimation(.easeIn(duration: 1.5)) {
-                            counter = (counter + 1) % images.count
-                        }
-                    }
-                    
-            
+            ForEach(0 ... images.count - 1, id: \.self) { idx in
+                if idx == counter {
+                    Image(images[idx]).resizable()
+                        .aspectRatio(contentMode: .fit).frame(height:250)
+                        .clipShape(.rect(cornerRadius: 10))
+                        .padding(10)
+                        .drawingGroup()
+                        .transition(self.diamondWave(size: 18))
+                        
+                }
+            }
+            Spacer()
         }
-        .toolbar {
-            ToggleAlphaButton(opacity: $opacity)
+        .onReceive(timer) { input in
+            withAnimation(.easeIn(duration: 1.5)) {
+                counter = (counter + 1) % images.count
+            }
         }
-        .navigationSubtitle("Shader")
     }
     
     public func diamondWave(size: Double = 20) -> AnyTransition {
@@ -55,29 +57,13 @@ struct ContentView: View {
     }
 }
 
-struct ToggleAlphaButton: View {
-    @Binding var opacity: Double
-
-    var body: some View {
-        Button("Toggle Opacity", systemImage: "cube.transparent") {
-            withAnimation {
-                if opacity.isZero {
-                    opacity = 1
-                } else {
-                    opacity = 0
-                }
-            }
-        }
-        .labelStyle(.titleAndIcon)
-    }
-}
 
 #Preview {
     ContentView()
 }
 
 /// A transition where many diamonds grow upwards to reveal the new content,
-/// with the diamonds moving outwards from the top-left edge.
+/// with the diamonds moving outwards from the bottom-right edge.
 @available(iOS 17, macOS 14, macCatalyst 17, tvOS 17, visionOS 1, *)
 struct DiamondWaveTransition: ViewModifier {
     /// How big to make the diamonds.
